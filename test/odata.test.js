b4c22f92f8988b0e0e8137bfae7755cdca6211ef
Daniel Hutzel <daniel.hutzel@sap.com>
2025-09-10 07:02:25 +0530

    fixed test

--- test/odata.test.js
+++ test/odata.test.js
@@ -1,5 +1,7 @@
 const cds = require('@sap/cds')
-const { GET, POST, PATCH, axios, expect } = cds.test(__dirname+'/..')
+cds.env.features.with_mocks = true
+
+const { GET, POST, PATCH, axios, expect } = cds.test(__dirname+'/..','--with-mocks')
 const EDIT = (url) => POST (url+'/TravelService.draftEdit',{})
 const SAVE = (url) => POST (url+'/TravelService.draftActivate')
 axios.defaults.auth = { username: 'alice', password: 'admin' }
