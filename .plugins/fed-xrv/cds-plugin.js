d37c5bb7e499b8d38daa60653895223dcde23e86
Daniel Hutzel <daniel.hutzel@sap.com>
2025-11-19 08:14:11 +0100

    Added support for cds.requires.srv.federation

--- .plugins/fed-xrv/cds-plugin.js
+++ .plugins/fed-xrv/cds-plugin.js
@@ -1,20 +1,30 @@
 const cds = require ('@sap/cds')
 const log = cds.log ('cds.data')
 
-if (cds.requires.data_federation) cds.on ('loaded', csn => {
 
-    const federated = e => e.is_entity && e['@federated']
-    const todos = cds.linked(csn).collect (federated, entity => {
-      let source = entity.source          // the external source entity
-      let srv = source.service            // the external service
-      let conf = cds.requires[srv?.name]  // the service's binding point
-      if (!conf?.credentials) return      // not bound to remote, skipping
-      entity['@cds.persistence.table'] = true
-      entity['@cds.persistence.skip'] = false
-      return { entity: entity.name, from: srv.name }
-    })
-    if (!todos.length) return
+cds.on ('loaded', csn => {
 
+    const todos = []
+    for (let each of cds.linked(csn).definitions) {
+      
+      // only entities tagged with @federated:true or @federated:'initial-load'
+      let fed = each.is_entity && each['@federated']
+      if (!fed || fed !== 'initial-load' && fed !== true) continue
+
+      // only bound services
+      let srv = each.source?.service
+      let conf = cds.requires[srv?.name]      
+      if (!conf?.credentials?.url) continue
+
+      // only if default federation config matches
+      if (fed === true && (conf.federation?.[each.name] || conf.federation?.default) !== 'initial-load') continue
+
+      // use consumption view as table for replication
+      each['@cds.persistence.table'] = true
+      todos.push ({ entity: each.name, from: srv.name })
+    }
+    if (!todos.length) return // nothing to do
+    
     cds.once ('served', async () => {
       const dfs = await cds.connect.to (DataFederationService)
       return Promise.all (todos.map (
@@ -35,8 +45,8 @@ class DataFederationService extends cds.Service { init() {
     const { entity, from: remote } = req.data
     const srv = await cds.connect.to (remote)
     const rows = await srv.read (entity) // @Bob: can we stream this?
-    await INSERT.into(entity).entries(rows)
-    log.info ('initially loaded:', { entity, from: remote, via: srv.kind })
+    await cds.insert(rows).into (entity)
+    log.info ('initially loaded', rows.length, 'rows for:', { entity, from: remote, via: srv.kind })
   })
 
 }}
