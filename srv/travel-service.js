8dbae1499f2fb5f37f26b30750ed4988d6c09c35
I548646 <paul.erlenwein@sap.com>
2025-12-08 11:30:03 +0100

    fix: adjust access property of undefined fix

--- srv/travel-service.js
+++ srv/travel-service.js
@@ -29,9 +29,9 @@ class TravelService extends cds.ApplicationService {
 
     // Inform XFlights about new bookings, so it can update occupied seats
     this.after ('SAVE', Travels, ({ Bookings }) => Promise.all (
-      Bookings.map (({ flightNumber, flightDate }) => xflights .send ('BookingCreated', {
+      Bookings?.map (({ flightNumber, flightDate }) => xflights .send ('BookingCreated', {
         flightNumber, flightDate
-      })
+      }) ?? []
     )))
   }
 
